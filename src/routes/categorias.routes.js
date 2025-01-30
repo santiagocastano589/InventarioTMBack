b06719b47b2fff70c205c1b0785e30f92e1db103
Santiago Castaño <santiagocastano589@gmail.com>
2025-01-30 16:22:25 -0500

    Correcion de ruta estado categoria

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -8,11 +8,10 @@ const {
   updateCategoryState,
 } = require('../controllers/catagorias.controller');
 
-
 router.get('/allCategorias', getCategorias);
 router.get('/categoria/:id', getCategoriaById);
 router.post('/newCategoria', createCategoria);
 router.put('/updateCategoria/:id', updateCategoria);
-router.delete('/updateEstadoCategoria/:id', updateCategoryState);
+router.put('/updateEstadoCategoria/:id', updateCategoryState);
 
 module.exports = router;
